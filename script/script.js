0683ee8d9c5eb4bfaad8933a7195522ebeb61480
Krzysztof Hamerszmit <kkhamer@gmail.com>
2017-02-26 10:17:57 +0100

    content + layout changes

--- script/script.js
+++ script/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+//WAYPOINTS
 var waypoint1 = new Waypoint({
 	element: document.getElementById('js-waypoint1'),
 	handler: function() {
@@ -10,3 +11,16 @@ var waypoint1 = new Waypoint({
 document.addEventListener('DOMContentLoaded', ()=> 
 	{ console.log('ready2');
 });
+
+
+//SCROLL
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+	let header = document.getElementById("header");
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        header.classList.add("header-fixed");
+    } else {
+        header.classList.remove("header-fixed");
+    }
+}
